af607defdcd8f2364880804bf5185d20b13173c9
Carlos Saltos <csaltos@gmail.com>
2022-11-20 06:41:04 -0500

    Initial code

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # k8s-saltos-school
+
+Instalación
+https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/install-kubeadm/
+
---
+++ installation/01-kubernetes-apt-repo.sh
@@ -0,0 +1,2 @@
+curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list
